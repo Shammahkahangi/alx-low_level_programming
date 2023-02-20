55bf9fca27c06d389fca69ec375ace05607e31a5
Shammahkahangi <shammahkahangi@gmail.com>
2023-02-20 15:19:01 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - A program that prints aline with the printf function
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+printf("ith proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
