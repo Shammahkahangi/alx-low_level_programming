a65273528f1c90818b211af4433780d4b3337e32
Shammahkahangi <shammahkahangi@gmail.com>
2023-02-20 18:46:45 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * DEscription 'Print Alphabet lowercase'
+ * REturn: Always 0
+ */
+int main(void)
+{int n = 97;
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar
+	return (0);
+
+}
