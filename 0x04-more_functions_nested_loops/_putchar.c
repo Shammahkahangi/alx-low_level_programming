88e0e4eedcfc83afaad9ac1bf8bf0e8394fbe16e
Shammahkahangi <shammahkahangi@gmail.com>
2023-02-23 14:33:28 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd>
+
+/**
+ * _putchar writes the characters c to stdout format
+ * @ The character to be printed
+ *
+ * Return ON succes 1
+ * on error -1 is returned
+ */
+int_putchar(char c)
+(
+ return(write(1, &C, 1))
+)
+
